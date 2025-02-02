aad26f4fe45e00f1404064176d9bd20e3471c6d0
matteo-rossi90 <matteo.rossi1314@gmail.com>
2025-02-02 21:04:34 +0100

    feat: modify app to test updating process in React

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root'))
+
+function renderApp() {
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
+
+setInterval( renderApp, 1000);
+
+
+
+
+
